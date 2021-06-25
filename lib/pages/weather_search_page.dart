dbd73b3066bb6df55c5a59ec46cf04813774b59f
Alperen Ekin <alperenekin97@gmail.com>
2021-06-25 15:48:39 +0300

    Cubit added to the search page and works fine

--- lib/pages/weather_search_page.dart
+++ lib/pages/weather_search_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_cubit_bloc_tutorial/cubit/weather_cubit.dart';
 import 'package:flutter_cubit_bloc_tutorial/data/model/weather.dart';
 
 class WeatherSearchPage extends StatefulWidget {
@@ -16,8 +18,27 @@ class _WeatherSearchPageState extends State<WeatherSearchPage> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 16),
         alignment: Alignment.center,
-        // TODO: Implement with cubit
-        child: buildInitialInput(),
+        child: BlocConsumer<WeatherCubit,WeatherState>( //cosumer = blocbuilder + blocklistener(this does not return widget but an action)
+          //cubit: BlocProvider.of<WeatherCubit>(context), don't need that because blocbuilder does for us
+          listener: (context,state){
+            if(state is WeatherError){
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text(state.message)));
+            }
+          },
+          builder: (context,state){
+            if(state is WeatherInitial){
+              return buildInitialInput();
+            }else if(state is WeatherLoading){
+              return buildLoading();
+            }else if(state is WeatherLoaded){
+              return buildColumnWithData(state.weather);
+            }else{
+              //Error state
+              return buildInitialInput();
+            }
+          },
+        ),
       ),
     );
   }
@@ -74,6 +95,7 @@ class CityInputField extends StatelessWidget {
   }
 
   void submitCityName(BuildContext context, String cityName) {
-    // TODO: Get weather for the city
+    final WeatherCubit weatherCubit = context.read<WeatherCubit>();
+    weatherCubit.getWeather(cityName);
   }
 }
